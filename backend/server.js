563c6d0867ddb33394535a2cac4be5e11aa96040
John Negrin <johnfrann31@gmail.com>
2024-01-22 20:35:09 -0500

    Section 4 Complete
    +Cleaned up routing
    +Added error middleware to help troubleshooting in long run

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,22 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-const port = process.env.port || 5000;
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-connectDB();    // Connect to MongoDB
+const port = process.env.PORT || 5000;
 
+connectDB();
 const app = express();
-
+app.use('/api/products', productRoutes);
 app.get('/', (req, res) => {
-    res.send('API is running...');
+  res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
-
-
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
