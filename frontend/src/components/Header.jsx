702eaeb0b49d9c6149a5dd9d92b50c1105034983
John Negrin <johnfrann31@gmail.com>
2024-01-28 17:54:49 -0500

    Section 6 - Shopping Cart Functionality
    +Added cart screen and cart slice
    +Added utils for cart
    +Began work on users

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import { Navbar, Nav, Container, Badge } from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
 import {LinkContainer} from 'react-router-bootstrap';
 import logo from '../assets/logo.png'
+import { useSelector } from 'react-redux';
 
 import React from 'react'
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -20,7 +22,12 @@ const Header = () => {
                     <Nav className="ms-auto">
                         <LinkContainer to="/cart">
                             <Nav.Link>
-                                <FaShoppingCart /> Cart 
+                            <FaShoppingCart /> Cart
+                                {cartItems.length > 0 && (
+                                <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                                {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                </Badge>
+                            )}
                             </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to="/login">
