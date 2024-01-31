e4c57c2c9668ad2bb07b9b21c69e5b98f00a2e7c
John Negrin <johnfrann31@gmail.com>
2024-01-30 23:15:44 -0500

    Section 8 Complete
    +Finalized login screen to provide login functionality
    +Added logout functionality
    +Added react-toastify and the form container
    +Added login and logout routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import {Outlet} from "react-router-dom"
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const App = () => {
   return (
     <>
-    <Header />
-    <main className="py-3">
-      <Container>
-        <Outlet/>
-      </Container>
-    </main>
-    <Footer></Footer>
+      <ToastContainer />
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
-}
-
+  );
+};
 export default App;
