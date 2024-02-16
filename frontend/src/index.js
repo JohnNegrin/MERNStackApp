b50048165fafa5c86dd65378298baae4efd2b9b7
John Negrin <johnfrann31@gmail.com>
2024-02-16 17:29:59 -0500

    Admin Functionality Pt 2
    +Fixed pictures
    +CRUD functionality for users and products

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -29,6 +29,8 @@ import OrderScreen from './screens/OrderScreen';
 import OrderListScreen from './screens/admin/OrderListScreen';
 import ProductListScreen from './screens/admin/ProductListScreen';
 import ProductEditScreen from './screens/admin/ProductEditScreen';
+import UserEditScreen from './screens/admin/UserEditScreen';
+import UserListScreen from './screens/admin/UserListScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -50,7 +52,9 @@ const router = createBrowserRouter(
       <Route path='' element={<AdminRoute />}>
         <Route path='/admin/orderlist' element={<OrderListScreen />} />
         <Route path='/admin/productlist' element={<ProductListScreen />} />
+        <Route path='/admin/userlist' element={<UserListScreen />} />
         <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
+        <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
       </Route>
     </Route>
   )
