9c0630e2f8448d9abb8a67bc8899c6625d333385
John Negrin <johnfrann31@gmail.com>
2024-03-25 19:02:03 -0400

    Small Fixes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import checkObjectId from '../middleware/checkObjectId.js';
 const router = express.Router();
 import {
   getProducts,
@@ -12,12 +13,13 @@ import {
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id/reviews').post(protect, createProductReview);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
+
 router.get('/top', getTopProducts);
 router
   .route('/:id')
-  .get(getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -65,7 +65,7 @@ const deleteHandler = async (id) => {
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>{error}</Message>
+        <Message variant='danger'>{error.data.message}</Message>
       ) : (
         <>
           <Table striped bordered hover responsive className='table-sm'>
